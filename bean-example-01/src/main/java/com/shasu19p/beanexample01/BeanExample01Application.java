da73d805be1d002f88633cf9141e9de5b9f364d0
Sunil Kumar Sharma <sunil-ss.sharma@broadcom.com>
2020-03-23 18:25:05 +0530

    Initialize bean example 1

---
+++ bean-example-01/src/main/java/com/shasu19p/beanexample01/BeanExample01Application.java
@@ -0,0 +1,13 @@
+package com.shasu19p.beanexample01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanExample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanExample01Application.class, args);
+	}
+
+}
---
+++ bean-example-01/src/test/java/com/shasu19p/beanexample01/BeanExample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shasu19p.beanexample01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanExample01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
